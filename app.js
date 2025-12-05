7e356074f315791f9c41501d7d363f8cce915f4e
Viljo <viljo.holma@gmail.com>
2025-12-05 21:09:27 +0200

    Make a little edit for testing purpose

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
@@ -18,3 +18,4 @@ app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
 
+// this should not trigger a pipeline
