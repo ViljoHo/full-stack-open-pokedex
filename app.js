cf78f32931dedccbd97929c33cd323c380b47c24
Viljo <viljo.holma@gmail.com>
2025-12-17 20:46:39 +0200

    Add #skip feature to pipeline

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('9') // change this string to ensure a new version deployed
+  res.send('10') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
@@ -17,5 +17,3 @@ app.get('/health', (req, res) => {
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
-
-// This is from taging feature
