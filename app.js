7f197c82389e7ea7640131dbf1b69f65525ec3f5
Viljo <viljo.holma@gmail.com>
2025-12-18 18:35:56 +0200

    Retry discord notifier

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('11') // change this string to ensure a new version deployed
+  res.send('12') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
@@ -18,4 +18,4 @@ app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
 
-// task 11.17 done now actually!
+// task 11.18 almost done
